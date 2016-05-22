d1bce32bf4c558cb7526364392b1daee634cd78a
marius92mc <marius_92m@yahoo.com>
2016-05-22 10:17:23 +0300

    Fixed a type in app.py

--- app.py
+++ app.py
@@ -28,12 +28,12 @@ def movie_ratings(user_id, movie_id):
     return json.dumps(ratings)
 
 
-@main.route("/<int:user_id>/ratings", methods = ["POST"])
+@main.route("/<int:user_id>/ratings", methods=["POST"])
 def add_ratings(user_id):
     # get the ratings from the Flask POST request object
     ratings_list = request.form.keys()[0].strip().split("\n")
     ratings_list = map(lambda x: x.split(","), ratings_list)
-    # create a list with the format required by the negine (user_id, movie_id, rating)
+    # create a list with the format required by the engine (user_id, movie_id, rating)
     ratings = map(lambda x: (user_id, int(x[0]), float(x[1])), ratings_list)
     # add them to the model using then engine API
     recommendation_engine.add_ratings(ratings)
